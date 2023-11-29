6b888999d4b0b88683159900dc18dc4e331660a5
Giorgi Megrelishvili <62292256+GiorgioMegrelli@users.noreply.github.com>
2023-11-30 00:05:24 +0400

    Implemented MkDir

--- src/main/java/ge/sh2/command/custom/mkdir/MkDir.java
+++ src/main/java/ge/sh2/command/custom/mkdir/MkDir.java
@@ -5,17 +5,26 @@ import ge.sh2.core.annotation.Parameters;
 import ge.sh2.core.command.CommandInvokable;
 import ge.sh2.core.command.ParametersWithArguments;
 
+import java.io.File;
+
 @Command(name = "mkdir", description = "Creates directories")
 public class MkDir implements CommandInvokable {
-
-    private static class MkDirParameters extends ParametersWithArguments {}
+    public static class MkDirParameters extends ParametersWithArguments {}
 
     @Parameters
     private MkDirParameters parameters;
 
     @Override
     public void invoke() throws Exception {
+        String[] args = parameters.getArguments();
+        if(args.length == 0) {
+            throw new RuntimeException("No argument is provided");
+        }
 
+        File dirName = new File(args[0]);
+        if(!dirName.exists()) {
+            dirName.mkdirs();
+        }
     }
 
 }
